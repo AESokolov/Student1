ea616a08c3e5c4476697b7c53dd2297a738830a6
Ginch <Ginch@192.168.56.1>
2018-05-25 11:36:29 +0300

    commit

---
+++ Student1/src/org/itstep/AppRunner.java
@@ -0,0 +1,19 @@
+package org.itstep;
+       import java.util.ArrayList;
+
+           public class AppRunner {
+
+	           public static void main(String[] args) {
+	        	   StudentService studentService = new StudentService();
+	       		StudentData[] studentName = studentService.getStudentInArray();
+	       		for(int i=0; i<studentName.length; i++) {	
+	       			System.out.println(studentName[i].getFirstName());
+	       		}
+	       		
+	       		ArrayList<StudentData> lists = studentService.getStudentInList();
+	       		for(StudentData list:lists) {
+	       			System.out.println(list.getSecondName());
+		}
+   }
+}
+           
